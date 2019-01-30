a59973c3dbf9a4feea367f645e8a28d13df69398
natashamorais <natasha.sousa.morais@gmail.com>
2019-01-30 02:27:36 -0200

    definição de rotas com angular e esqueleto do site com bootstrap

--- teste/src/app/app.module.ts
+++ teste/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
